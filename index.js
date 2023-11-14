c4d973e9c2fdfed0011f0e2b60d00c7b627b12dc
Neel-05 <72031915+Neel-05@users.noreply.github.com>
2023-11-14 10:45:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello, This is devops project by avik, syed and soumik'); //write a response to the client
+  res.write('Hello everyone, This is devops project by avik, syed and soumik'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
